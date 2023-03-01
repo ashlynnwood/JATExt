0df349aecfc978b83f582eadfb36c1813c07ec60
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-01 14:55:59 -0600

    Debugged, everything working let's gooooo

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,45 +13,30 @@ const initdb = async () =>
     },
   });
 
-// Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  // Create connection to db and version 
   const jateDb = await openDB('jate', 1);
-  // Create transaction and specify db and data privileges
   const tx = jateDb.transaction('jate', 'readwrite');
-  // Open up object store
   const store = tx.objectStore('jate');
-  // Add content to db
-  const request = store.put({ id: id, jate: content });
-  // Confirmation of request
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
-  console.error('putDb not implemented');
-}
+  console.log('🚀 - data saved to the database', result.value);
+};
 
-// Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
   console.log('GET from the database');
-
-  // Create connection to db and version 
   const jateDb = await openDB('jate', 1);
-
-  // Create transaction and specify db and data privileges
   const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up object store
   const store = tx.objectStore('jate');
-
-  // Get all data 
-  const request = store.getAll();
-
-  // Get confirmation of the request
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
-  console.error('getDb not implemented');
-  return result;
-
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,8 +35,8 @@ module.exports = () => {
         description: 'Just Another Text Editor',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
@@ -52,7 +52,7 @@ module.exports = () => {
         // CSS loader
         {
           test: /\.css$/i,
-          use: ['style.loader', 'css-loader'],
+          use: ['style-loader', 'css-loader'],
         },
         // Babel to use ES6
         {
